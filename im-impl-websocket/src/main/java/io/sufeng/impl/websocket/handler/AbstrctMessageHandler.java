8aa0d8f05eb172177efee1a55f3277b2d53adc96
zhangchao <jaguar_zc@sina.com>
2019-12-20 15:00:22 +0800

    feat(db) : 连接开发库

--- im-impl-websocket/src/main/java/io/sufeng/impl/websocket/handler/AbstrctMessageHandler.java
+++ im-impl-websocket/src/main/java/io/sufeng/impl/websocket/handler/AbstrctMessageHandler.java
@@ -3,25 +3,41 @@ package io.sufeng.impl.websocket.handler;
 import com.google.gson.Gson;
 import io.netty.channel.ChannelHandlerContext;
 import io.netty.channel.ChannelInboundHandlerAdapter;
-import io.sufeng.impl.websocket.message.ErrorMsg;
 import io.sufeng.impl.websocket.message.Msg;
+
+import java.util.Iterator;
+import java.util.ServiceLoader;
 
 /**
  * @description:
  * @author: sufeng
  * @create: 2019-11-22 17:06
  */
-public abstract class AbstrctMessageHandler extends ChannelInboundHandlerAdapter {
+public abstract class AbstrctMessageHandler extends ChannelInboundHandlerAdapter implements MessageHandler {
+
+    Iterator<MessageHandler>  messageHandlerServiceLoader;
+    Iterator<MessageDecoderErrorHandler> messageDecoderErrorHandlerIterator;
+    {
+        messageHandlerServiceLoader = ServiceLoader.load(MessageHandler.class).iterator();
+        messageDecoderErrorHandlerIterator = ServiceLoader.load(MessageDecoderErrorHandler.class).iterator();
+    }
 
     @Override
     public void channelRead(ChannelHandlerContext ctx, Object msg) throws Exception {
         super.channelRead(ctx, msg);
         try{
-            Msg msg1 = new Gson().fromJson(msg.toString(), Msg.class);
-            onMessage(ctx,msg1);
+            while(messageHandlerServiceLoader.hasNext()) {
+                MessageHandler messageHandler = messageHandlerServiceLoader.next();
+                Msg msg1 = new Gson().fromJson(msg.toString(), Msg.class);
+                boolean result = messageHandler.onMessage(ctx, msg1);
+                if(result){
+                    break;
+                }
+            }
         }catch (Exception e){
-            ctx.writeAndFlush(new ErrorMsg(e.getMessage()));
+            while (messageDecoderErrorHandlerIterator.hasNext()){
+                messageDecoderErrorHandlerIterator.next().onErrorMessage(ctx,msg.toString());
+            }
         }
     }
-    public abstract void onMessage(ChannelHandlerContext ctx,Msg t);
 }
